ddee61f0ff534c3aa87446e2f726fed014d4837e
Sam E <samhithenganti@gmail.com>
2024-04-27 21:18:48 -0400

    added IDT

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ pub extern "C" fn _start() -> ! {
 
     sigmaos::init();
 
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
 
     #[cfg(test)]
     test_main();
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,13 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 #![test_runner(sigmaos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-use sigmaos::{println, serial_print, serial_println};
+use sigmaos::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -16,16 +14,13 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    sigmaos::test_panic_handler(info)
+}
+
 #[test_case]
 fn test_println() {
     println!("test_println output");
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    sigmaos::test_panic_handler(info)
-}
